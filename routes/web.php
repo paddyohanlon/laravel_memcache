ac4932761b7b87bb7409c3589e588fade5ab74ae
Paddy <ohanlon.paddy@gmail.com>
2023-02-13 13:32:01 +0100

    Try disable caching again

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Task;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+// use Illuminate\Support\Facades\Cache;
 
 /*
 |--------------------------------------------------------------------------
